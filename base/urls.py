06b79f32b3d37766ba2bb32e9e8afd8b4d714395
fennin3 <enninfrancis47@gmail.com>
2020-10-01 21:10:08 +0000

    UI updated

--- base/urls.py
+++ base/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 
 urlpatterns = [
-    path('',views.home, name="home")
+    path('',views.home, name="home"),
+    path('contact-us/', views.contact_us, name='contact_us'),
+    path('services/', views.services, name='services')
 ]
--- base/views.py
+++ base/views.py
@@ -4,3 +4,9 @@ from django.shortcuts import render
 
 def home(request):
     return render(request,'base/index.html')
+
+def contact_us(request):
+    return render(request, "base/contact_us.html")
+
+def services(request):
+    return render(request, 'base/services.html')
